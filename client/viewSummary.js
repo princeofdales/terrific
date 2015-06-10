39d81cca94df9eca1b66977872cff78e5081ce58
Tom Berman <tjcberman@gmail.com>
2015-06-10 15:17:44 +0100

    clean up

--- client/viewSummary.js
+++ client/viewSummary.js
@@ -1,13 +1,6 @@
-
-
 Template.viewSummary.helpers({
   projects: function(){
     console.log(this);
     return ExtraProjects.find({companyId:this._id});
-  },
-  bob:function(){
-    console.log(this);
-    return null;
   }
-
 });
